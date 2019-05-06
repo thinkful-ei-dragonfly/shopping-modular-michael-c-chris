04d37225d28ce726aba86e78cb355e7cae12f10a
cfiander <cfiander90@gmail.com>
2019-05-06 09:22:03 -0700

    item changes

--- scripts/item.js
+++ scripts/item.js
@@ -1,7 +1,21 @@
 const Item = (function() {
     const foo = 'bar'; 
+    function validateName(name) {
+        if (!name) {
+        throw new TypeError('Name does not exist.');
+        }
+    }
+    
+    function create(name) {
+        return {
+            id: cuid(),
+            name,
+            checked: false
+        };
+    }
     return {
-        foo
+        validateName,
+        create
     };
 } ());
 
