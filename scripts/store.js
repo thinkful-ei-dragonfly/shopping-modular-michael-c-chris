82b0266afc8e0ce9e8179884feac231b8b25a9ae
m-cal <iengineersoftware@gmail.com>
2019-05-06 11:38:45 -0400

    completed first set of drills

---
+++ scripts/store.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const store = (function() {
+  let foo = 'bar';
+  const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+
+  let hideCheckedItems  = false;
+  let searchTerm = '';
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm,
+    foo,
+  }
+}() );
+
